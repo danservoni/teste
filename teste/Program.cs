38fbc9848c5358c24eb098e750b1b41870e999ea
Danilo Servoni <danservoni@gmail.com>
2020-07-24 06:42:52 -0300

    Foi acrescentado a mensagem de bom dia!

--- teste/Program.cs
+++ teste/Program.cs
@@ -7,6 +7,7 @@ namespace teste
         static void Main(string[] args)
         {
             Console.WriteLine("Olá mundo!");
+            Console.WriteLine("Bom dia!");
 
             Console.ReadLine();
         }
