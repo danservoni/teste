33188d197026c900c3af57acb0d644e2763ef21a
Danilo Servoni <danservoni@gmail.com>
2020-07-24 06:41:01 -0300

    Programa executa a mensagem Ola Mundo

---
+++ teste/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo!");
+
+            Console.ReadLine();
+        }
+    }
+}
